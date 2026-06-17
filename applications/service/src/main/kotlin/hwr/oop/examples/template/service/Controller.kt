00c25a20b9ca974f7b8a9717690398a2127dbf76
henri <s_nemeth25@stud.hwr-berlin.de>
2026-06-17 14:53:41 +0200

    stefan complained about test and missing inline constructor args

--- applications/service/src/main/kotlin/hwr/oop/examples/template/service/Controller.kt
+++ applications/service/src/main/kotlin/hwr/oop/examples/template/service/Controller.kt
@@ -8,6 +8,7 @@ import hwr.oop.examples.template.core.GameInstance
 import hwr.oop.examples.template.core.GamePendingChoice
 import hwr.oop.examples.template.core.Pile
 import hwr.oop.examples.template.core.Player
+import hwr.oop.examples.template.core.PlayerId
 import hwr.oop.examples.template.service.api.GameActionApi
 import hwr.oop.examples.template.service.api.GameApi
 import hwr.oop.examples.template.service.model.*
@@ -17,147 +18,150 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class Controller(
-	val persistence: DominionPersistence
+    val persistence: DominionPersistence
 ) : GameApi, GameActionApi {
 
-	override fun getGame(gameId: String?): ResponseEntity<GameState> {
-		require(gameId != null) { "Game ID is null" }
-		val game = persistence.load(gameId)
-		return map(game)
-	}
+    override fun getGame(gameId: String?): ResponseEntity<GameState> {
+        require(gameId != null) { "Game ID is null" }
+        val game = persistence.load(gameId)
+        return map(game)
+    }
 
-	private fun map(game: GameInstance): ResponseEntity<GameState> {
-		val state = GameState(game.id(),
-			game.status(),
-			game.currentPlayerId(),
-			game.currentPhase(),
-			game.actionsRemaining(),
-			game.buysRemaining(),
-			game.coinsAvailable(),
-			mapSupplies(game.supply()),
-			mapPlayers(game.players()),
-			game.effect().mapEffect())
-		return ResponseEntity.ok(state)
-	}
+    private fun map(game: GameInstance): ResponseEntity<GameState> {
+        val state = GameState(
+            /* gameId = */ game.id(),
+            /* status = */ game.status(),
+            /* currentPlayerId = */ game.currentPlayerId(),
+            /* currentPhase = */ game.currentPhase(),
+            /* actionsRemaining = */ game.actionsRemaining(),
+            /* buysRemaining = */ game.buysRemaining(),
+            /* coinsAvailable = */ game.coinsAvailable(),
+            /* supply = */ mapSupplies(game.supply()),
+            /* players = */ mapPlayers(game.players()),
+            /* effectStack = */ game.effect().mapEffect()
+        )
+        return ResponseEntity.ok(state)
+    }
 
-	private fun mapSupplies(piles: Set<Pile>): List<SupplyPile> {
-		return piles.map{ SupplyPile(it.name(), it.count()) }
-	}
+    private fun mapSupplies(piles: Set<Pile>): List<SupplyPile> {
+        return piles.map { SupplyPile(it.name(), it.count()) }
+    }
 
-	private fun mapPlayers(players: List<Player>): List<PlayerState> {
-		return players.map { player ->
-			PlayerState(
-				player.id().value,
-				player.currentHand().map { it.toString() },
-				player.playArea(),
-				player.currentDiscard().map { it.toString() },
-				player.deckSize()
-			)
-		}
-	}
+    private fun mapPlayers(players: List<Player>): List<PlayerState> {
+        return players.map { player ->
+            PlayerState(
+                player.id().value,
+                player.currentHand().map { it.toString() },
+                player.playArea(),
+                player.currentDiscard().map { it.toString() },
+                player.deckSize()
+            )
+        }
+    }
 
-	private fun CardEffect.mapEffect(): List<ActiveEffect> {
-		return remainingSteps()
-			.mapIndexed { index, step ->
-				ActiveEffect(
-					card.name,
-					instigatingPlayer().value,
-					step.explanation,
-					if (index == 0)
-						pending.map { mapChoice(it) }
-					else
-						emptyList()
-				)
-			}
-	}
+    private fun CardEffect.mapEffect(): List<ActiveEffect> {
+        return remainingSteps()
+            .mapIndexed { index, step ->
+                ActiveEffect(
+                    card.name,
+                    instigatingPlayer().value,
+                    step.explanation,
+                    if (index == 0)
+                        pending.map { mapChoice(it) }
+                    else
+                        emptyList()
+                )
+            }
+    }
 
-	override fun startGame(startGameRequest: @Valid StartGameRequest?): ResponseEntity<GameCreatedResponse> {
-		require(startGameRequest != null) { "startGameRequest must not be null" }
-		val players = startGameRequest.playerIds
-		val kingdomCards = startGameRequest.kingdomCards
-		val game = GameInstance.create(players.toList(), kingdomCards.toList())
-		persistence.save(game)
-		return ResponseEntity.ok(GameCreatedResponse(game.id()))
-	}
-	
-	override fun buyCard(
-		gameId: String?,
-		buyCardsRequest: @Valid BuyCardsRequest?,
-	): ResponseEntity<GameState> {
-		require(gameId != null) { "Game ID is required" }
-		require(buyCardsRequest != null) { "buyCardsRequest is required" }
+    override fun startGame(startGameRequest: @Valid StartGameRequest?): ResponseEntity<GameCreatedResponse> {
+        require(startGameRequest != null) { "startGameRequest must not be null" }
+        val players = startGameRequest.playerIds
+        val kingdomCards = startGameRequest.kingdomCards
+        val game = GameInstance.create(players.toList(), kingdomCards.toList())
+        persistence.save(game)
+        return ResponseEntity.ok(GameCreatedResponse(game.id()))
+    }
 
-		val game = persistence.load(gameId)
-		require(game.isActivePlayer(buyCardsRequest.playerId)) { "player ${buyCardsRequest.playerId} is not the active player" }
-		buyCardsRequest.cardsToBuy
+    override fun buyCard(
+        gameId: String?,
+        buyCardsRequest: @Valid BuyCardsRequest?,
+    ): ResponseEntity<GameState> {
+        require(gameId != null) { "Game ID is required" }
+        require(buyCardsRequest != null) { "buyCardsRequest is required" }
 
-		return map(game.purchase(buyCardsRequest.cardsToBuy))
-	}
+        val game = persistence.load(gameId)
+        require(game.isActivePlayer(buyCardsRequest.playerId)) { "player ${buyCardsRequest.playerId} is not the active player" }
+        buyCardsRequest.cardsToBuy
 
-	fun mapChoice(choice: GamePendingChoice): PendingChoice {
-		return PendingChoice(
-			choice.playerId.value,
-			choice.choiceType,
-			choice.description,
-			choice.options,
-			choice.minSelections,
-			choice.maxSelections
-		)
-	}
+        return map(game.purchase(buyCardsRequest.cardsToBuy))
+    }
 
-	override fun getChoices(gameId: String?): ResponseEntity<PendingChoicesResponse> {
-		require(gameId != null) { "Game ID is required" }
-		val game = persistence.load(gameId)
+    fun mapChoice(choice: GamePendingChoice): PendingChoice {
+        return PendingChoice(
+            choice.playerId.value,
+            choice.choiceType,
+            choice.description,
+            choice.options,
+            choice.minSelections,
+            choice.maxSelections
+        )
+    }
 
-		val choices = game.choices()
-		val pendingChoices = choices.map {
-			mapChoice(it)
-		}
+    override fun getChoices(gameId: String?): ResponseEntity<PendingChoicesResponse> {
+        require(gameId != null) { "Game ID is required" }
+        val game = persistence.load(gameId)
 
-		return ResponseEntity.ok(PendingChoicesResponse(pendingChoices))
-	}
-	
-	override fun makeChoice(
-		gameId: String?,
-		makeChoiceRequest: @Valid MakeChoiceRequest?,
-	): ResponseEntity<GameState> {
-		require(gameId != null) { "Game ID is required" }
-		require(makeChoiceRequest != null) { "makeChoiceRequest must not be null" }
+        val choices = game.choices()
+        val pendingChoices = choices.map {
+            mapChoice(it)
+        }
 
-		val game = persistence.load(gameId)
-		require(game.isActivePlayer(makeChoiceRequest.playerId)) { "player ${makeChoiceRequest.playerId} is not the active player" }
+        return ResponseEntity.ok(PendingChoicesResponse(pendingChoices))
+    }
 
-		val result = game.makeChoice(AnsweredChoice(makeChoiceRequest.playerId, makeChoiceRequest.selectedOptions))
-		return map(result)
-	}
-	
-	override fun playAction(
-		gameId: String?,
-		playActionRequest: @Valid PlayActionRequest?,
-	): ResponseEntity<GameState> {
-		require(gameId != null) { "Game ID is required" }
-		require(playActionRequest != null) { "playActionRequest is required" }
+    override fun makeChoice(
+        gameId: String?,
+        makeChoiceRequest: @Valid MakeChoiceRequest?,
+    ): ResponseEntity<GameState> {
+        require(gameId != null) { "Game ID is required" }
+        require(makeChoiceRequest != null) { "makeChoiceRequest must not be null" }
 
-		val game = persistence.load(gameId)
-		require(game.isActivePlayer(playActionRequest.playerId)) { "player ${playActionRequest.playerId} is not the active player" }
+        val game = persistence.load(gameId)
+        require(game.isActivePlayer(makeChoiceRequest.playerId)) { "player ${makeChoiceRequest.playerId} is not the active player" }
 
-		val result = game.playAction(playActionRequest.cardName)
+        val result =
+            game.makeChoice(AnsweredChoice(PlayerId(makeChoiceRequest.playerId), makeChoiceRequest.selectedOptions))
+        return map(result)
+    }
 
-		return map(result)
-	}
-	
-	override fun playTreasures(
-		gameId: String?,
-		playTreasuresRequest: @Valid PlayTreasuresRequest?,
-	): ResponseEntity<GameState> {
-		require(gameId != null) { "Game ID is required" }
-		require(playTreasuresRequest != null) { "playTreasuresRequest is required" }
+    override fun playAction(
+        gameId: String?,
+        playActionRequest: @Valid PlayActionRequest?,
+    ): ResponseEntity<GameState> {
+        require(gameId != null) { "Game ID is required" }
+        require(playActionRequest != null) { "playActionRequest is required" }
 
-		val game = persistence.load(gameId)
-		require(game.isActivePlayer(playTreasuresRequest.playerId)) { "player ${playTreasuresRequest.playerId} is not the active player" }
+        val game = persistence.load(gameId)
+        require(game.isActivePlayer(playActionRequest.playerId)) { "player ${playActionRequest.playerId} is not the active player" }
 
-		val result = game.playTreasures(playTreasuresRequest.cardNames)
-		return map(result)
-	}
-	
+        val result = game.playAction(playActionRequest.cardName)
+
+        return map(result)
+    }
+
+    override fun playTreasures(
+        gameId: String?,
+        playTreasuresRequest: @Valid PlayTreasuresRequest?,
+    ): ResponseEntity<GameState> {
+        require(gameId != null) { "Game ID is required" }
+        require(playTreasuresRequest != null) { "playTreasuresRequest is required" }
+
+        val game = persistence.load(gameId)
+        require(game.isActivePlayer(playTreasuresRequest.playerId)) { "player ${playTreasuresRequest.playerId} is not the active player" }
+
+        val result = game.playTreasures(playTreasuresRequest.cardNames)
+        return map(result)
+    }
+
 }
