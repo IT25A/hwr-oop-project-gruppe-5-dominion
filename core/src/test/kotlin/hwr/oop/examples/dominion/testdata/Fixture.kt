47d44f63719ecea311e372b9e968d8e120a3411c
Sohrab Matini Majd <s_matinimajd25@stud.hwr-berlin.de>
2026-07-09 22:47:13 +0200

    chore added Fixture.kt without data

---
+++ core/src/test/kotlin/hwr/oop/examples/dominion/testdata/Fixture.kt
@@ -0,0 +1,27 @@
+package hwr.oop.examples.dominion.testdata
+
+//import hwr.oop.examples.dominion.PlayerId
+import hwr.oop.examples.dominion.BoardState
+
+object Fixture {
+
+    val alpha = "alpha" // PlayId hinzufügen witchig !!!!!!
+    val beta = "beta"
+    val gamma = "gamma"
+    val delta = "delta"
+
+//    val BoardState = listOf(
+
+//    )
+fun main(args : Array<String>) {
+    println("Hello, World!")
+}
+
+
+//    fun game(): Game = Game(
+//        handsOfPlayers = hands,
+//        bouts = pastBoutsFinished,
+//    )
+
+//    private infix fun PlayerId.withHand(cards: List<Card>): Hand = Hand(this, cards)
+}
