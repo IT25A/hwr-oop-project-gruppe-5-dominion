e06b7a0076666042c468c016d8e16b2d6f36794d
henri <s_nemeth25@stud.hwr-berlin.de>
2026-07-11 12:11:55 +0200

    now uses validate method instead of validating per rest endpoint

--- applications/service/src/main/kotlin/hwr/oop/examples/template/service/Controller.kt
+++ applications/service/src/main/kotlin/hwr/oop/examples/template/service/Controller.kt
@@ -90,9 +90,7 @@ class Controller(
         require(gameId != null) { "Game ID is required" }
         require(buyCardsRequest != null) { "buyCardsRequest is required" }
 
-        val game = persistence.load(gameId)
-        require(game.isActivePlayer(buyCardsRequest.playerId)) { "player ${buyCardsRequest.playerId} is not the active player" }
-        buyCardsRequest.cardsToBuy
+        val game = persistence.load(gameId).validate(buyCardsRequest.playerId)
 
         return map(game.purchase(buyCardsRequest.cardsToBuy))
     }
@@ -127,11 +125,9 @@ class Controller(
         require(gameId != null) { "Game ID is required" }
         require(makeChoiceRequest != null) { "makeChoiceRequest must not be null" }
 
-        val game = persistence.load(gameId)
-        require(game.isActivePlayer(makeChoiceRequest.playerId)) { "player ${makeChoiceRequest.playerId} is not the active player" }
+        val game = persistence.load(gameId).validate(makeChoiceRequest.playerId)
 
-        val result =
-            game.makeChoice(AnsweredChoice(PlayerId(makeChoiceRequest.playerId), makeChoiceRequest.selectedOptions))
+        val result = game.makeChoice(AnsweredChoice(PlayerId(makeChoiceRequest.playerId), makeChoiceRequest.selectedOptions))
         return map(result)
     }
 
@@ -142,11 +138,9 @@ class Controller(
         require(gameId != null) { "Game ID is required" }
         require(playActionRequest != null) { "playActionRequest is required" }
 
-        val game = persistence.load(gameId)
-        require(game.isActivePlayer(playActionRequest.playerId)) { "player ${playActionRequest.playerId} is not the active player" }
+        val game = persistence.load(gameId).validate(playActionRequest.playerId)
 
         val result = game.playAction(playActionRequest.cardName)
-
         return map(result)
     }
 
@@ -157,8 +151,7 @@ class Controller(
         require(gameId != null) { "Game ID is required" }
         require(playTreasuresRequest != null) { "playTreasuresRequest is required" }
 
-        val game = persistence.load(gameId)
-        require(game.isActivePlayer(playTreasuresRequest.playerId)) { "player ${playTreasuresRequest.playerId} is not the active player" }
+        val game = persistence.load(gameId).validate(playTreasuresRequest.playerId)
 
         val result = game.playTreasures(playTreasuresRequest.cardNames)
         return map(result)
