b3a16d37b9e611d2f21e9adcadc53b0bc0b29a2a
henri <s_nemeth25@stud.hwr-berlin.de>
2026-07-11 12:09:08 +0200

    onGame command now takes persistence as context object

--- applications/cli/src/main/kotlin/hwr/oop/examples/template/cli/OnGameIdCommand.kt
+++ applications/cli/src/main/kotlin/hwr/oop/examples/template/cli/OnGameIdCommand.kt
@@ -3,11 +3,14 @@ package hwr.oop.examples.template.cli
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.obj
 import com.github.ajalt.clikt.parameters.arguments.argument
+import hwr.oop.examples.dominion.DominionPersistence
 
-class OnGameIdCommand : CliktCommand(name = "onGameID") {
+class OnGameIdCommand(
+	private val persistence: DominionPersistence
+) : CliktCommand(name = "onGameID") {
 	private val gameId by argument("GAMEID", help = "The unique identifier of the game.")
 	
 	override fun run() {
-		currentContext.obj = gameId
+		currentContext.obj = persistence.load(gameId)
 	}
 }
